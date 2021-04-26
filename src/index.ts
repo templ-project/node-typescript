dec9aea830b8457fafe0dc13562a5912a818c2f0
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-04-27 00:33:09 +0300

    +

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,53 @@
 export const hello = (name: string): string => `Hello ${name}!`;
+
+const curlyTest = (a, b, c, d) => {
+  if (a < 0) {a++;}
+  else {console.log(foo);}
+
+  while (a === true) {console.log('test');}
+
+  for (const x of [a, b, c, d]) {console.log(x);}
+};
+
+function returnValue(a) {
+  // Noncompliant
+  if (a === 1) {
+    return true;
+  }
+
+  if (a.indexOf('blue') > 0) {
+    return 'test';
+  }
+
+  switch (a) {
+  case 0:
+    doSomething();
+    break;
+  default:
+    doSomethingElse();
+    break;
+  }
+
+  return 3;
+}
+
+const eslintTest = async (a, b, c, d) => {
+  console.log(a, b, c, d);
+
+  curlyTest();
+  returnValue();
+
+  {
+    const x = undefined;
+    if (x === NaN) {
+      console.log('is NaN');
+    }
+  }
+
+  {
+    const x = 42;
+    await x;
+  }
+
+  alert('test');
+};
